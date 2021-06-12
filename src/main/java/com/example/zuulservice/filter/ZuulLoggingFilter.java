dca91b3ae2bf85513b3d86331c301779945cafd5
Wan seok Park <wandol@Wanui-MacBookPro.local>
2021-06-13 01:18:09 +0900

    Initial commit

---
+++ src/main/java/com/example/zuulservice/filter/ZuulLoggingFilter.java
@@ -0,0 +1,40 @@
+package com.example.zuulservice.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@Slf4j
+public class ZuulLoggingFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        log.info("=========== printing log: ");
+
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        log.info("=========== " + request.getRequestURI());
+
+        return null;
+    }
+}
